299dda02e1d427f6c9143910c1277297910dba2a
Jeff LeBoeuf <jeff@jeff.how>
2023-05-26 21:09:05 -0400

    contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,12 +1,12 @@
-import { get_csrf, submit_contact } from './api.js';
+// import { submit_contact } from './paladinmail.js';
+
+
 const contact_modal = document.querySelector('#contact-modal');
 const contact_modal_close = document.querySelector('#contact-modal-close');
 contact_modal_close.addEventListener('click', closeContactModal);
 
-const csrf = document.querySelector( '[name="csrf_token"]' );
-csrf.value = await get_csrf();
-
 const contact_form = document.querySelector( '#contact-form' );
+
 contact_form.addEventListener( 'submit', submit_form );
 
 async function submit_form(e) {
@@ -14,10 +14,23 @@ async function submit_form(e) {
     e.preventDefault();
     showContactModal();
     
-    const formData = new URLSearchParams( new FormData( this ) );
-    await submit_contact(formData);
+    const formData = new URLSearchParams( new FormData( contact_form ) );
+    
+    const client_id = 'Uh7dL&&f$w8P&kwhjCQsuP8ysTf$ut12';
+    const api_endpoint = 'https://api.paladinwebgroup.com/';
+    fetch( api_endpoint, {
+        method:'POST',
+        // mode: 'no-cors',
+        headers: {
+            "Client-Id": client_id
+        },
+        body: formData 
+    })
+    // debug
+    // .then( response => console.log(response.text()) ) 
+    // .then( data => console.log(data))
+    .catch( err => console.log(err) )
 
-        
 }
 
 function showContactModal(){
